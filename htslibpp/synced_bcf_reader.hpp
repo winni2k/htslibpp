0d24d68e78221d707e5ba9d002d76bc3035392f7
Warren W. Kretzschmar <wkretzsch@gmail.com>
2015-06-03 22:36:39 +0100

    Adds more tweaks to get indexed reading to work

--- htslibpp/synced_bcf_reader.hpp
+++ htslibpp/synced_bcf_reader.hpp
@@ -9,7 +9,28 @@
 #include <stdexcept>
 #include <string>
 
+namespace bcf_srs_helper {
+
+struct init {
+  std::string region;
+  bool useIndex = false;
+};
+}
+
 class bcf_srs {
+private:
+    void set_target(const std::string &region) {
+    if (bcf_sr_set_targets(m_sr, region.c_str(), false, 0) != 0)
+      throw std::runtime_error("Could not set target: [" + region + "]");
+    assert(m_sr->require_index == 0);
+  }
+  void set_region(const std::string &region) {
+
+    if (bcf_sr_set_regions(m_sr, region.c_str(), false) != 0)
+      throw std::runtime_error("Could not set region: [" + region + "]");
+    assert(m_sr->require_index == 1);
+  }
+
 protected:
   bcf_srs_t *m_sr = nullptr;
   size_t m_nFiles = 0;
@@ -20,12 +41,18 @@ public:
       bcf_sr_destroy(m_sr);
   }
   bcf_srs() { m_sr = bcf_sr_init(); }
-  bcf_srs(const std::string &region) : bcf_srs() { set_region(region); }
+  bcf_srs(bcf_srs_helper::init &init) : bcf_srs() {
+    if (!init.region.empty()) {
+      if (init.useIndex)
+        set_region(init.region);
+      else
+        set_target(init.region);
+    }
+  }
 
   bcf_srs_t *data() { return m_sr; }
 
   void add_reader(const std::string &filename) {
-    m_sr->require_index = tbx_index_load(filename.c_str()) != nullptr;
     if (bcf_sr_add_reader(m_sr, filename.c_str()) != 1)
       throw std::runtime_error("Error while adding file: [" + filename + "]");
     ++m_nFiles;
@@ -52,16 +79,6 @@ public:
     if (fileNum >= m_nFiles)
       throw std::range_error("fileNum is too large");
     return bcf_sr_get_line(m_sr, fileNum);
-  }
-
-  void set_target(const std::string &region) {
-    if (bcf_sr_set_targets(m_sr, region.c_str(), false, 0) != 0)
-      throw std::runtime_error("Could not set target: [" + region + "]");
-  }
-  void set_region(const std::string &region) {
-
-    if (!all_indexed() || bcf_sr_set_regions(m_sr, region.c_str(), false) != 0)
-      throw std::runtime_error("Could not set region: [" + region + "]");
   }
 
   bcf_hdr_t *get_header(size_t fileNum) {
